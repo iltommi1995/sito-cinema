58c152a0dacde3dec25e05351ec11057d0655f33
Allan Nava <allannava95@gmail.com>
2020-01-08 22:59:59 +0100

    aggiornato il json degli orari di biccocca e la nuova struttura

--- src/components/orariFilm/orariFilmBicocca.js
+++ src/components/orariFilm/orariFilmBicocca.js
@@ -2,29 +2,22 @@
 * Author    :   Tomàs Avila - Allan Nava
 * Modified  :   Tomàs Avila - Allan Nava  
 * Created   :   07/01/2020
-* Updated   :   07/01/2020
+* Updated   :   08/01/2020
 */
 import React, {Component} from 'react';
 import FilmData from '../../data/filmBicocca';
 import "../../css/components/schedaFilm/schedaFilm.css";
-
-import SchedaOrariFilm from "./schedaOrariFilm";
-
-
-
+//
+import SchedaOrariFilmV2 from "./schedaOrariFilmV2";
+//
 class OrariFilmBicocca extends Component {
     render() {
         return(
             <div>
                 <h2>Cinema Milano bicocca </h2>
 
-                {FilmData.map((filmDetail, index)=>
-                        <SchedaOrariFilm
-                            film={{img: filmDetail.img, id:filmDetail.id, regista:filmDetail.regista, genere:filmDetail.genere,
-                                durata:filmDetail.durata, cast:filmDetail.cast, titolo:filmDetail.titolo, sinossi:filmDetail.sinossi,
-                                orariprimo: filmDetail.orari.primo, orarisecondo:filmDetail.orari.secondo,
-                                orariterzo:filmDetail.orari.terzo, orariquarto:filmDetail.orari.quarto}}/>
-                )}
+                {
+                    FilmData.map(filmDetail =><SchedaOrariFilmV2 film={filmDetail} />)}
             </div>
         )
     }
---
+++ src/components/orariFilm/schedaOrariFilmV2.js
@@ -0,0 +1,62 @@
+import React from "react";
+import {Col, Row} from "reactstrap";
+import ModalExample from "../schedaFilm/schedaFilm";
+
+function schedaGiorni (data, day) {
+    return(
+        <div>
+            <div className="giorno">
+                <h4 className="h4-giorno">{day}</h4>
+            </div>
+            <div>
+                <p className="orario-film">{data.primo}</p>
+                <p className="orario-film">{data.secondo}</p>
+                <p className="orario-film">{data.terzo}</p>
+                <p className="orario-film">{data.quarto}</p>
+            </div>
+        </div>
+    )
+}
+
+function SchedaOrariFilmV2 (data) {
+    return(
+        <container>
+            <Row className="scheda-film">
+                <Col xs="3">
+                    
+                    <img src={data.film.img} alt="immagine" className="img-scheda-film"/>
+                </Col>
+                <Col xs="3" className="colonna-dati-film">
+                    <h3>{data.film.titolo}</h3>
+                    <br/>
+                    <p><b>Regia:</b> {data.film.regista}</p>
+                    <p><b>Genere:</b> {data.film.genere}</p>
+                    <p><b>Durata:</b> {data.film.durata}</p>
+                    <p><b>Cast:</b> {data.film.cast}</p>
+                    <ModalExample
+                        buttonLabel="Scheda del Film"
+                        nomeFilm={data.film.titolo}
+                        regista={data.film.regista}
+                        genere={data.film.genere}
+                        durata={data.film.durata}
+                        cast={data.film.cast}
+                        sinossi={data.film.sinossi}
+                        immagine={data.film.img}
+                    />
+                </Col>
+                <Col xs="6" className="colonna-orari">
+                    <Row>
+                    { Object.keys(data.film.orari).map(key =>
+                        schedaGiorni(data.film.orari[key], key)
+                )}
+
+                    </Row>
+                </Col>
+            </Row>
+
+        </container>
+
+    )
+}
+
+export default SchedaOrariFilmV2;
