eb4e5811e1962dcca14bb365d576405efa7001d4
iltommi1995 <59200709+iltommi1995@users.noreply.github.com>
2020-01-08 15:09:56 +0100

    asdd

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -9,7 +9,15 @@ import React, {Component} from 'react'
 class Home extends Component {
     render() {
         return(
-            <p>SONO LA HOME</p>
+            <div>
+                <h2>BENVENUTI DA SDV CINEMAS</h2>
+                <p>SDV Cinemas è una catena di multisala in espansione, con al momento due complessi, entrambi a Milano.
+                    Con una distribuzione variegata SDV Cinemas cerca di accontentare tutti i tipi di spettatori.
+                    Presso il multisala SDV Cinema- Bicocca, si possono vedere i maggiori film del momento.
+                    SDV Cinema- Centrale invece può vantare una ricercata selezione di film d'essai.
+                </p>
+                <p>Visita le nostre sale e scopri la programmazione:</p>
+            </div>
         )
     }
 }
