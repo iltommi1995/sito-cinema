835c71da38c9246eca49aedeee587d3486892704
iltommi1995 <59200709+iltommi1995@users.noreply.github.com>
2020-01-10 15:49:22 +0100

    asda

--- src/components/mainLayout/footer/footer.js
+++ src/components/mainLayout/footer/footer.js
@@ -6,9 +6,48 @@ import "../../../css/components/mainLayout/footer/footer.css";
 
 function Footer() {
     return(
-        <div id="footer">
-            <h3 id="scrittaFooter">Sono il footer.</h3>
-        </div>
+        <footer>
+            <div className="container-fluid padding">
+                <div className="row text-center">
+                    <div className="col-md-4">
+                        <h3>SDV CINEMAS</h3>
+                        <hr className="light"></hr>
+                        <p>
+                            123-456-789
+                        </p>
+                        <p>
+                            Email
+                        </p>
+                        <p>
+                            Indirizzo
+                        </p>
+                        <p>
+                            Codice postale
+                        </p>
+                    </div>
+                    <div className="col-md-4">
+                        <hr className="light"></hr>
+                        <h5>I NOSTRI ORARI</h5>
+                        <hr className="light"></hr>
+                        <p>Blablabla</p>
+                        <p>Blablabla</p>
+                        <p>Blablabla</p>
+                    </div>
+                    <div className="col-md-4">
+                        <hr className="light"></hr>
+                        <h5>I NOSTRI ORARI</h5>
+                        <hr className="light"></hr>
+                        <p>Blablabla</p>
+                        <p>Blablabla</p>
+                        <p>Blablabla</p>
+                    </div>
+                    <div className="col-12">
+                        <hr className="light"></hr>
+                        <h5>© SDVCinemas</h5>
+                    </div>
+                </div>
+            </div>
+        </footer>
     )
 }
 
