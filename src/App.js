8ce126f994966fdb5dcf3d65c18e3526b249a815
iltommi1995 <59200709+iltommi1995@users.noreply.github.com>
2020-01-24 19:26:02 +0100

    orologio

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import "./css/general.css";
 import Header from "./components/mainLayout/header/header"
 import Footer from "./components/mainLayout/footer/footer";
 import MainTemplate from "./components/mainLayout/mainTemplate/mainTemplate";
+import Orologio from "./components/orologio/orologio";
+import Timer from "./components/timer/timer";
 
 
 
@@ -17,6 +19,12 @@ function App() {
               <div className="container" >
                   <MainTemplate/>
               </div>
+          </div>
+          <div style={{"position": "fixed", "top": "100px", "right": "50px"}}>
+            <Orologio />
+          </div>
+          <div style={{"position": "fixed", "top": "100px", "left": "50px"}}>
+              <Timer />
           </div>
           <Footer />
       </div>
